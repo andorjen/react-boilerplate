ab015ee2a656b3c85c2ba631a723ca9a93795daf
Jenny Liu <jennyliuye@gmail.com>
2022-02-03 13:55:22 -0800

    add snapshot for Homepage

--- app/containers/HomePage/tests/index.test.js
+++ app/containers/HomePage/tests/index.test.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { render } from 'react-testing-library';
-import { IntlProvider } from 'react-intl';
-
+// import { IntlProvider } from 'react-intl';
+import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import history from 'utils/history';
 import HomePage from '../index';
+import configureStore from '../../../configureStore';
+
+// Create redux store with history
+const initialState = {};
+const store = configureStore(initialState, history);
 
 describe('<HomePage />', () => {
   it('should render and match the snapshot', () => {
-    const {
-      container: { firstChild },
-    } = render(
-      <IntlProvider locale="en">
-        <HomePage />
-      </IntlProvider>,
+    const { container } = render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <HomePage />
+        </MemoryRouter>
+      </Provider>,
     );
-    expect(firstChild).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
   });
 });
